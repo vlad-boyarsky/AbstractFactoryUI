e16dcb8aa852d9da1c912385d39881ba71fb9525
vladkage <hokage.vlados@gmail.com>
2021-07-05 18:48:41 +0300

    Core

--- index.php
+++ index.php
@@ -4,3 +4,8 @@ namespace App;
 
 require __DIR__ . '/vendor/autoload.php';
 
+$userPackageFactory = new UserPackageFactory;
+
+$package = $userPackageFactory->getPackageFactory('vip');
+
+echo $package->connectClassicBackground();
