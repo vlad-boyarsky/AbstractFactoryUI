06bfecd3ff2bb5d7d30f9ba8357064c66f9214a9
vladkage <hokage.vlados@gmail.com>
2021-07-05 18:31:11 +0300

    Core

--- App/UserPackageFactory.php
+++ App/UserPackageFactory.php
@@ -3,13 +3,30 @@
 
 namespace App;
 
+use App\Factories\StandardPackageFactory;
+use App\Factories\VipPackageFactory;
 use App\Interfaces\UserPackageFactoryInterface;
 
 class UserPackageFactory
 {
-    public function getPackageFactory(): UserPackageFactoryInterface
+
+    private UserPackageFactoryInterface $factory;
+
+    public function getPackageFactory($selected): UserPackageFactoryInterface
     {
-        
+        $factory = $this->factory;
+
+        switch ($selected) {
+            case 'standard':
+                $factory = new StandardPackageFactory;
+                break;
+
+            case 'vip':
+                $factory = new VipPackageFactory;
+                break;
+        }
+
+        return $factory;
     }
 
 }
